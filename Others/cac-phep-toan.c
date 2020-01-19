cb191a03df38f36a0c6a5ebf3edf6c382473526a
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 15:18:24 +0700

    Create cac-phep-toan.c

---
+++ Others/cac-phep-toan.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int a,b;
+	printf("Nhap vao so a: ");
+	scanf("%d",&a);
+	printf("Nhap vao so b: ");
+	scanf("%d",&b);
+	printf("___________________\n");
+	printf("Tong la: %d+%d=%d\n",a,b,a+b);
+	printf("Hieu la: %d-%d=%d\n",a,b,a-b);
+	printf("Tich la: %d*%d=%d\n",a,b,a*b);
+	printf("Thuong la: %d/%d=%d\n",a,b,a/b);
+}
