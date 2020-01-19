e305e34015dbd39f8620530ecfa4534ee8517eab
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 15:18:20 +0700

    Create ax+b.c

---
+++ Others/ax+b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+float a,b,kq;
+
+printf("GIAI PHUONG TRINH BAC NHAT DANG ax+b=0\n");
+printf("Nhap a= ");
+scanf("%f",&a);
+printf("Nhap b= ");
+scanf("%f",&b);
+kq=-(b/a);
+printf("Ket qua la: %0.1f",kq);
+}
