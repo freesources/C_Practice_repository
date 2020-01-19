2aa4fac014527ff2d59afaef537d3cc6c66b470f
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 15:18:50 +0700

    Create S_CV_tron.c

---
+++ Others/S_CV_tron.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+	float R,S,CV;
+	printf("Nhap vao ban kinh R: ");
+	scanf("%f",&R);
+	printf("__________________________\n");
+	printf("Chu vi la: %f\n",2*3.14*R);
+	printf("Dien tich la: %f\n",3.14*R*R);
+}
