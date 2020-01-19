c8a7ede41a482825aa21db628ade030a03468363
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 15:19:06 +0700

    Create Tinh_so_tuoi.c

---
+++ Others/Tinh_so_tuoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int n,t;
+void main()
+{
+	printf("Nhap nam sinh nguoi do:\n");
+	scanf("%d",&n);
+	printh("nhap nam hien tai:\n");
+	scanf("%d",&t)
+	printf("So tuoi nguoi do la:%d",t);
+	
+}
